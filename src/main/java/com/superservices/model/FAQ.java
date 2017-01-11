fecf7fcb237847bac97750cf675ed9eac64d1b07
anil <anilugale1989@gmail.com>
2017-01-11 13:56:57 +0530

    added new model FAQ class

---
+++ src/main/java/com/superservices/model/FAQ.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.superservices.model;
+
+/**
+ *
+ * @author anil
+ */
+public class FAQ {
+    
+}
